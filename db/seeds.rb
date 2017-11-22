29c69494e6a94b79d4c7836472bb284f4b940a0b
Alix de Bricourt <alix.debricourt@gmail.com>
2017-11-22 15:43:17 +0100

    Add Restaurant index & show views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating restaurants...'
+Restaurant.create!({
+  name: "Le Dindon en Laisse",
+  address: "18 Rue Beautreillis, 75004 Paris, France"
+  phone_number: "0240950064"
+  category: "chinese"
+})
+Restaurant.create!({
+  name: "Neuf et Voisins",
+  address: "Van Arteveldestraat 1, 1000 Brussels, Belgium"
+  phone_number: "0240950064"
+  category: "japanese"
+})
+Restaurant.create!({
+  name: "La cuisine de Mamy",
+  address: "Van Arteveldestraat 1, 1000 Brussels, Belgium"
+  phone_number: "0140950064"
+  category: "italian"
+})
+Restaurant.create!({
+  name: "Tartelette et trompette",
+  address: "Van Arteveldestraat 1, 1000 Brussels, Belgium"
+  phone_number: "0640950064"
+  category: "french"
+})
+Restaurant.create!({
+  name: "Pique et croque",
+  address: "Van Arteveldestraat 1, 1000 Brussels, Belgium"
+  phone_number: "0440950064"
+  category: "belgian"
+})
+puts 'Finished!'
